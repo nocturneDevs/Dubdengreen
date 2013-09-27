fa4c4cca2b61c655db5d2268c7060f3eec6c3c48
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-09-28 02:50:53 +0530

    Less is better
    Decided to pack the 2013 theme as we are only attempting to visually
    create the site at the moment.
    We can move to a better boilerplate when we undertake actual
    development.

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
  * Displays all of the <head> section and everything up till <div id="main">
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
@@ -19,33 +19,30 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
+
+<!-- Scalable Vector Icons from Font Awesome -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+<!-- FONTS --><!-- TODO: Is this the best way to do this? --> 
+<link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Mako:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lato:300,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:300' rel='stylesheet' type='text/css'>
+<script src="http://use.edgefonts.net/aguafina-script:n4.js"></script>
+
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-	<div id="page" class="hfeed site">
-		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
-
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
-		</header><!-- #masthead -->
-
-		<div id="main" class="site-main">
+<body>
+	<div id="page">
--- index.php
+++ index.php
@@ -1,38 +1,73 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme and one of the
- * two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+	<div id="title-wrapper">
+		<h1><a href="<?php echo home_url(); ?>">Softrade</a></h1>
+		<h3>25 years of Code</h3>
+	</div>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
+	<nav id="main-nav">
+		<a href="<?php echo home_url(); ?>">Home</a>
+		<a href="<?php echo home_url(); ?>">Portfolio</a>
+		<a href="<?php echo home_url(); ?>">Contact Us</a>
+	</nav>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+	<div id="main-description" class="homepage-section">
+		<p id="main-description-graphic">
+			25
+		</p>
+		<p id="main-description-text">
+			For twenty five years, Softrade has been in the business of delivering quality software tailored to the needs of its customers. From elegant end to end solutions for chit funds and family owned businesses to huge cash management systems for corporates like Deutsche Bank, Softrade has carved a niche for itself in delivering robust and flexible customised software solutions. That much of our software is still in use even 15 years after it was written stands testament to the stamp of quality on each of these carefully crafted products.
+		</p>
+	</div>
 
-			<?php twentythirteen_paging_nav(); ?>
+	<div id="mantras" class="homepage-section">
+		
+		<div class="a-mantra">
+			<h4>Bulletproof systems</h4>
+			<p>
+				It just works. We're not kidding!
+			</p>
+		</div>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
+		<div class="a-mantra">
+			<h4>Perfect, snug fit</h4>
+			<p>
+				No ready-made stuff. We know the importance of writing our code from scratch for each of our customers.
+			</p>
+		</div>
+		
+		<div class="a-mantra">
+			<h4>Small is big</h4>
+			<p>
+				We believe in churning out tailored masterpieces rather than make big bucks on volumes that help no one.
+			</p>
+		</div>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+		<div class="a-mantra">
+			<h4>Quality quality quality</h4>
+			<p>
+				Our clients have stood by us for decades now and that's because we believe in quality.
+			</p>
+		</div>
 
-<?php get_sidebar(); ?>
+		<div class="a-mantra">
+			<h4>Always on time</h4>
+			<p>
+				We've been defying the industry standard for 25 years.
+			</p>
+		</div>
+
+	</div>
+
+
+	<div id="clients" class="homepage-section">
+		<h4>Our Clients</h4>
+		<div id="clientLogos">
+			<img src="<?php echo get_template_directory_uri(); ?>/resources/images/clientLogos/DB.jpg">
+			<img src="<?php echo get_template_directory_uri(); ?>/resources/images/clientLogos/SCB.jpg">
+			<img src="<?php echo get_template_directory_uri(); ?>/resources/images/clientLogos/Total.jpg">
+			<img src="<?php echo get_template_directory_uri(); ?>/resources/images/clientLogos/SKS.jpg">
+		</div>
+	</div>
+	
 <?php get_footer(); ?>
